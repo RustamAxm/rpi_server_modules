4721432db9d74989b787471ab60b7e4313317ec1
Rustam Akhmadullin <r.akhmadullin@1440.space>
2023-06-01 15:09:51 +0300

    add adc

--- server_zmq.py
+++ server_zmq.py
@@ -4,7 +4,7 @@ import time
 import zmq
 import socket
 import yaml
-
+import Adafruit_ADS1x15
 from modules.ch341_linux_api import SmBusWrapper
 
 
@@ -15,6 +15,14 @@ def write_to_i2c(onet, message):
     except Exception as err:
         print(f'{err} in write to bus')
         return err.args
+
+
+def get_adc_data(adc, gain=1):
+    data = []
+    for i in range(4):
+        data.append(adc.read_adc(i, gain=gain))
+
+    return data
 
 
 def start_sever():
@@ -33,21 +41,30 @@ def start_sever():
         config = yaml.full_load(conf)
         print(f"i2c bus = {config['bus']}, address = {config['address']}")
 
+    adc = None
+    try:
+        adc = Adafruit_ADS1x15.ADS1015(address=0x48, busnum=config['bus'])
+    except Exception:
+        print(f"just server i2c-ADC {config['bus']} or 0x48 not found")
+
+    onet = None
     try:
         onet = SmBusWrapper(bus_=config['bus'], address=config['address'])
-        status_ = True
     except Exception:
         print(f"just server i2c {config['bus']} or {config['address']} not found")
-        status_ = False
 
     while True:
         #  Wait for next request from client
         message = json.loads(socket_zmq.recv_json())
         print(f"Received request: {message}")
 
-        dict_to_send = {'out': None}
-        if status_:
-            dict_to_send['out'] = write_to_i2c(onet, message)
+        dict_to_send = {'onet_out': None,
+                        'ads_out': None}
+        if onet is not None:
+            dict_to_send['onet_out'] = write_to_i2c(onet, message)
+
+        if adc is not None:
+            dict_to_send['ads_out'] = get_adc_data(adc)
 
         time.sleep(0.2)
         #  Send reply back to client
